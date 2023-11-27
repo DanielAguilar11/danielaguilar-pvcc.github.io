f7d75a3fce6d564f695a5810113f6bec46af50d9
danielaguilar-pvcc <145607455+danielaguilar-pvcc@users.noreply.github.com>
2023-11-27 15:16:41 -0500

    Add files via upload

--- custreport.py
+++ custreport.py
@@ -10,12 +10,12 @@ def main():
 
 def read_in_cust_file():
     cust_data = open("customer_data_file.csv", "r") 
-    cust in cust_data.readlines ()
+    cust_in = cust_data.readlines()
     cust_data.close()
 
     #split the data into fields 
-      for i in cust_in:
-          cust.append(i.split(","))
+    for i in cust_in:
+        cust.append(i.split(","))
 
 def display_cust_list():
 
@@ -26,9 +26,9 @@ def display_cust_list():
     print(line)
     print("***** CUSTOMER SALES REPORT ********\n")
     for i in range(len(cust)):
-        amt owed float (cust[i] [2]) 1.10 #increase amount by 10% 
-        total+amt owed
-        print(cust[i][1] +      "\t" + cust[i][0]+ "\t"+ format(amt_owed, fcurrency))
+        amt_owed = float (cust[i][2]) * 1.10 #increase amount by 10% 
+        total += amt_owed
+        print(cust[i][1] +      "\t" + cust[i][0]+ "\t"+ format(amt_owed,fcurrency))
 
     print(line)
     print("**** TOTAL AMOUNT:\t$" + format (total, fcurrency))
